a1e95f826d2ffa0b9993f62054b8d03d1cbee283
usabarashi <usabarashi@mac.com>
2023-07-22 13:27:07 +0000

    Add Exercise 3.24.

---
+++ rust/src/part1/chapter3/tree.rs
@@ -0,0 +1,50 @@
+use std::cmp::max;
+use std::rc::Rc;
+
+enum Tree<A> {
+    Leaf {
+        value: A,
+    },
+    Branch {
+        left: Rc<Tree<A>>,
+        right: Rc<Tree<A>>,
+    },
+}
+
+impl<A> Tree<A> {
+    fn size(&self) -> usize {
+        match self {
+            Tree::Leaf { .. } => 1,
+            Tree::Branch { left, right } => 1 + left.size() + right.size(),
+        }
+    }
+}
+
+impl Tree<i32> {
+    fn maximum(&self) -> i32 {
+        match self {
+            Tree::Leaf { value } => value.clone(),
+            Tree::Branch { left, right } => max(left.maximum(), right.maximum()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_exercise324() {
+        let tree = Tree::Branch {
+            left: Rc::new(Tree::Branch {
+                left: Rc::new(Tree::Leaf { value: 1 }),
+                right: Rc::new(Tree::Leaf { value: 2 }),
+            }),
+            right: Rc::new(Tree::Branch {
+                left: Rc::new(Tree::Leaf { value: 3 }),
+                right: Rc::new(Tree::Leaf { value: 4 }),
+            }),
+        };
+        assert_eq!(tree.maximum(), 4);
+    }
+}
