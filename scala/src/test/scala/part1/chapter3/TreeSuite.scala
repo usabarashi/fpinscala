006f4583f08ff554151b3c99ac5174d64f97c64e
usabarashi <usabarashi@mac.com>
2023-07-22 14:28:56 +0000

    Refactor

--- scala/src/test/scala/part1/chapter3/TreeSuite.scala
+++ scala/src/test/scala/part1/chapter3/TreeSuite.scala
@@ -4,7 +4,7 @@ import org.scalatest.Assertions._
 import part1.chapter3.Tree.*
 
 class TreeSuite extends munit.FunSuite {
-  test("Exercise 3.24") {
+  test("Exercise 3.25") {
     assertEquals(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4))).maximum, 4)
   }
 }
