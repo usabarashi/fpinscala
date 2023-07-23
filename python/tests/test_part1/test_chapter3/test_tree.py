88881ed3f53dcf04a6c680c21ff7b6f1fc7e2f55
usabarashi <usabarashi@mac.com>
2023-07-23 00:41:05 +0000

    Refactor

--- python/tests/test_part1/test_chapter3/test_tree.py
+++ python/tests/test_part1/test_chapter3/test_tree.py
@@ -4,7 +4,7 @@ from part1.chapter3.tree import Tree, Leaf, Branch
 
 def test_exercise325():
     tree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
-    assert tree.maximum() == 4
+    assert 4 == tree.maximum()
 
 def test_exercise326():
     tree = Branch(Leaf(1), Branch(Leaf(2), Branch(Leaf(3), Leaf(3))))
