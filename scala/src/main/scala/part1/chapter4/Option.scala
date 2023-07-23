7c282b53080d6917f68282974970e033ae9be16e
usabarashi <usabarashi@mac.com>
2023-07-23 06:20:35 +0000

    Add Exercise 4.1.

---
+++ scala/src/main/scala/part1/chapter4/Option.scala
@@ -0,0 +1,27 @@
+
+package part1.chapter4
+
+import scala.{Option as _, Some as _, None as _}
+
+enum Option[+A]:
+    case Some(get: A)
+    case None
+
+    def map[B](f: A => B): Option[B] =
+        this match
+            case None => None
+            case Some(value) => Some(f(value))
+
+    def flatMap[B](f: A => Option[B]): Option[B] =
+        map(f).getOrElse(None)
+
+    def getOrElse[B >: A](default: => B): B =
+        this match
+            case None => default
+            case Some(value) => value
+
+    def orElse[B >: A](ob: => Option[B]): Option[B] =
+        map(Some(_)).getOrElse(ob)
+
+    def filter(f: A => Boolean): Option[A] =
+        flatMap(value => if f(value) then Some(value) else None)
---
+++ scala/src/test/scala/part1/chapter4/OptionSuite.scala
@@ -0,0 +1,21 @@
+import scala.{Option as _, Some as _, None as _}
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.Assertions._
+
+import part1.chapter4.Option.*
+
+class OptionSuite extends munit.FunSuite {
+  test("Exercise 4.1") {
+    //assertEquals(None.map(_ + 1), None)
+    assertEquals(Some(42).map(_ + 1), Some(43))
+    //assertEquals(None.flatMap(_ + 1), None)
+    assertEquals(Some(42).flatMap((value) => Some(value + 1)), Some(43))
+    assertEquals(None.getOrElse(0), 0)
+    assertEquals(Some(42).getOrElse(0), 42)
+    assertEquals(None.orElse((() => Some(42))()), Some(42))
+    assertEquals(Some(42).orElse((()=> Some(0))()), Some(42))
+    //assertEquals(None.filter(_ <= 42), None)
+    assertEquals(Some(42).filter(_ <= 42), Some(42))
+  }
+}
