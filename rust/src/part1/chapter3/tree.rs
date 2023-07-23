140f1f118bfc650a9bc86b6b936f9ba263607f40
usabarashi <usabarashi@mac.com>
2023-07-23 02:43:13 +0000

    Add Exercise 3.28.

--- rust/src/part1/chapter3/tree.rs
+++ rust/src/part1/chapter3/tree.rs
@@ -45,6 +45,38 @@ where
             },
         }
     }
+
+    fn fold<B, F, G>(&self, f: F, g: G) -> B
+    where
+        F: Fn(A) -> B + Copy,
+        G: Fn(B, B) -> B + Copy,
+    {
+        match self {
+            Tree::Leaf { value } => f(value.clone()),
+            Tree::Branch { left, right } => g(left.fold(f, g), right.fold(f, g)),
+        }
+    }
+
+    fn size_via_fold(&self) -> usize {
+        self.fold(|_| 1, |x, y| 1 + x + y)
+    }
+
+    fn depth_via_fold(&self) -> usize {
+        self.fold(|_| 0, |left, right| 1 + max(left, right))
+    }
+
+    fn map_via_fold<B, F>(&self, f: F) -> Tree<B>
+    where
+        F: Fn(A) -> B + Copy,
+    {
+        self.fold(
+            |value| Tree::Leaf { value: f(value) },
+            |left, right| Tree::Branch {
+                left: Rc::new(left),
+                right: Rc::new(right),
+            },
+        )
+    }
 }
 
 impl Tree<i32> {
@@ -121,5 +153,40 @@ mod tests {
             }),
         };
         assert_eq!(tree.map(|x| x.to_string()), expect);
+    }
+
+    #[test]
+    fn test_exercise328() {
+        let tree = Tree::Branch {
+            left: Rc::new(Tree::Leaf { value: 1 }),
+            right: Rc::new(Tree::Branch {
+                left: Rc::new(Tree::Leaf { value: 2 }),
+                right: Rc::new(Tree::Branch {
+                    left: Rc::new(Tree::Leaf { value: 3 }),
+                    right: Rc::new(Tree::Leaf { value: 3 }),
+                }),
+            }),
+        };
+        assert_eq!(tree.size_via_fold(), 7);
+        assert_eq!(tree.depth_via_fold(), 3);
+        let expect = Tree::Branch {
+            left: Rc::new(Tree::Leaf {
+                value: "1".to_string(),
+            }),
+            right: Rc::new(Tree::Branch {
+                left: Rc::new(Tree::Leaf {
+                    value: "2".to_string(),
+                }),
+                right: Rc::new(Tree::Branch {
+                    left: Rc::new(Tree::Leaf {
+                        value: "3".to_string(),
+                    }),
+                    right: Rc::new(Tree::Leaf {
+                        value: "3".to_string(),
+                    }),
+                }),
+            }),
+        };
+        assert_eq!(tree.map_via_fold(|x| x.to_string()), expect);
     }
 }
