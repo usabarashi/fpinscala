2760b6bd53323963c7654e4397d21afdf5198913
usabarashi <usabarashi@mac.com>
2023-07-23 01:19:38 +0000

    Add Exercise 3.27.

--- rust/src/part1/chapter3/tree.rs
+++ rust/src/part1/chapter3/tree.rs
@@ -1,6 +1,7 @@
 use std::cmp::max;
 use std::rc::Rc;
 
+#[derive(Debug, Clone, PartialEq)]
 enum Tree<A> {
     Leaf {
         value: A,
@@ -11,7 +12,10 @@ enum Tree<A> {
     },
 }
 
-impl<A> Tree<A> {
+impl<A> Tree<A>
+where
+    A: Clone,
+{
     fn size(&self) -> usize {
         match self {
             Tree::Leaf { .. } => 1,
@@ -23,6 +27,22 @@ impl<A> Tree<A> {
         match self {
             Tree::Leaf { .. } => 0,
             Tree::Branch { left, right } => 1 + max(left.depth(), right.depth()),
+        }
+    }
+
+    fn map<B, F>(&self, f: F) -> Tree<B>
+    where
+        B: Clone,
+        F: Fn(A) -> B + Copy,
+    {
+        match self {
+            Tree::Leaf { value } => Tree::Leaf {
+                value: f(value.clone()),
+            },
+            Tree::Branch { left, right } => Tree::Branch {
+                left: Rc::new(left.map(f)),
+                right: Rc::new(right.map(f)),
+            },
         }
     }
 }
@@ -68,5 +88,38 @@ mod tests {
             }),
         };
         assert_eq!(tree.depth(), 3);
+    }
+
+    #[test]
+    fn test_exercise327() {
+        let tree = Tree::Branch {
+            left: Rc::new(Tree::Leaf { value: 1 }),
+            right: Rc::new(Tree::Branch {
+                left: Rc::new(Tree::Leaf { value: 2 }),
+                right: Rc::new(Tree::Branch {
+                    left: Rc::new(Tree::Leaf { value: 3 }),
+                    right: Rc::new(Tree::Leaf { value: 3 }),
+                }),
+            }),
+        };
+        let expect = Tree::Branch {
+            left: Rc::new(Tree::Leaf {
+                value: "1".to_string(),
+            }),
+            right: Rc::new(Tree::Branch {
+                left: Rc::new(Tree::Leaf {
+                    value: "2".to_string(),
+                }),
+                right: Rc::new(Tree::Branch {
+                    left: Rc::new(Tree::Leaf {
+                        value: "3".to_string(),
+                    }),
+                    right: Rc::new(Tree::Leaf {
+                        value: "3".to_string(),
+                    }),
+                }),
+            }),
+        };
+        assert_eq!(tree.map(|x| x.to_string()), expect);
     }
 }
