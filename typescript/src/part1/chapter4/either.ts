dad99dbdbcf8c8e62ef89c122fa4bc946a556424
usabarashi <usabarashi@mac.com>
2023-09-01 08:09:25 +0000

    Add Exercise 4.7.

--- typescript/src/part1/chapter4/either.ts
+++ typescript/src/part1/chapter4/either.ts
@@ -10,13 +10,13 @@ export interface Right<R> {
     value: R
 }
 
-export const map = <L, R, RR>(v: Either<L, R>, f: (v: R) => RR): Either<L, RR> =>
+export const map = <L, A, B>(v: Either<L, A>, f: (v: A) => B): Either<L, B> =>
     match(v)
         .with({ type: 'Left' }, (left) => ({ ...left }))
         .with({ type: 'Right' }, (right) => ({ ...right, value: f(right.value) }))
         .exhaustive()
 
-export const flatMap = <L, R, RR>(v: Either<L, R>, f: (v: R) => Either<L, RR>): Either<L, RR> =>
+export const flatMap = <L, A, B>(v: Either<L, A>, f: (v: A) => Either<L, B>): Either<L, B> =>
     match(v)
         .with({ type: 'Left' }, (left) => ({ ...left }))
         .with({ type: 'Right' }, (right) => f(right.value))
@@ -28,5 +28,16 @@ export const orElse = <L, LL, R>(v: Either<L, R>, f: () => Either<LL, R>): Eithe
         .with({ type: 'Right' }, (right) => right)
         .exhaustive()
 
-export const map2 = <L, R1, R2, R3>(a: Either<L, R1>, b: Either<L, R2>, f: (a: R1, b: R2) => R3): Either<L, R3> =>
+export const map2 = <L, A, B, C>(a: Either<L, A>, b: Either<L, B>, f: (a: A, b: B) => C): Either<L, C> =>
     flatMap(a, (a) => map(b, (b) => f(a, b)))
+
+export const sequence = <L, R>(xs: Array<Either<L, R>>): Either<L, Array<R>> =>
+    traverse(xs, (x) => x)
+
+export const traverse = <L, A, B>(xs: Array<A>, f: (v: A) => Either<L, B>): Either<L, Array<B>> =>
+    match(xs)
+        .with([], () => ({ type: 'Right', value: [] }) as Either<L, Array<B>>)
+        .otherwise((xs) => {
+            const [head, ...tail] = xs
+            return map2(f(head), traverse(tail, f), (h, t) => [h].concat(t))
+        })
--- typescript/tests/part1/chapter4/either.test.ts
+++ typescript/tests/part1/chapter4/either.test.ts
@@ -1,11 +1,11 @@
 import {
     Either
-    , Left
-    , Right
     , map
     , flatMap
     , orElse
     , map2
+    , sequence
+    , traverse
 } from 'src/part1/chapter4/either'
 
 describe("Exercise 4.6", () => {
@@ -17,4 +17,11 @@ describe("Exercise 4.6", () => {
     test("orElse Right", () => expect(orElse({ type: 'Right', value: 42 }, () => ({ type: 'Left', value: "error" }))).toStrictEqual({ type: 'Right', value: 42 }))
     test("map2 Left", () => expect(map2({ type: 'Left', value: "error" }, { type: "Left", value: "error" }, (a, b) => a + b)).toStrictEqual({ type: 'Left', value: "error" }))
     test("map2 Right", () => expect(map2({ type: 'Right', value: 42 }, { type: "Right", value: 42 }, (a, b) => a + b)).toStrictEqual({ type: 'Right', value: 84 }))
+})
+
+describe("Exercise 4.7", () => {
+    test("sequence Left", () => expect(sequence([{ type: 'Right', value: 42 }, { type: 'Left', value: "error" } as Either<string, number>])).toStrictEqual({ type: 'Left', value: "error" }))
+    test("sequence Right", () => expect(sequence([{ type: 'Right', value: 42 }, { type: 'Right', value: 42 }])).toStrictEqual({ type: 'Right', value: [42, 42] }))
+    test("traverse Left", () => expect(traverse([{ type: 'Right', value: 42 } as Either<string, number>, { type: 'Left', value: "error" } as Either<string, number>], x => x)).toStrictEqual({ type: 'Left', value: "error" }))
+    test("traverse Right", () => expect(traverse([{ type: 'Right', value: 42 } as Either<string, number>, { type: 'Right', value: 42 } as Either<string, number>], x => x)).toStrictEqual({ type: 'Right', value: [42, 42] }))
 })
